62e7edacfa20615a2d854cc00045ea1f2a688835
Alina <alinarybalchenko@hotmail.com>
2025-09-20 23:30:40 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import fetchData from './js/pixabay-api';
 import moduleName from './js/render-functions';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+
+getImagesByQuery('cats').then(data => console.log(data.hits));
