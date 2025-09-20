8b3ad4e59ac108534a6b8e92c8082d4eacc864bb
Alina <alinarybalchenko@hotmail.com>
2025-09-21 01:14:33 +0200

    resolve problem

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = import.meta.env.VITE_PIXABAY_KEY;
+const API_KEY = '52389108-9de8a863b1caabc6aa6df9d02';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export function getImagesByQuery(query) {
